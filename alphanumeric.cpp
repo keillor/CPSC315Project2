fb0155ea62e6cc14807f2319a9f05933ae92dbe9
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2024-03-11 19:21:00 -0700

    finalized comments and polished.

--- alphanumeric.cpp
+++ alphanumeric.cpp
@@ -3,20 +3,19 @@
 #include <pthread.h>
 #include <string.h>
 #include <ctype.h>
-//#include <iostream>
+// isdigit()
+// isalpha()
 
 
 int currentIndex = 1;
-int arrayLength;
+int arrayLength; //argc
 
 void *numeric(char* myStr[]) {
     int internalIndex;
     while (currentIndex < arrayLength) {
         internalIndex = currentIndex;
-        //myStr[internalIndex] > 47 && myStr[internalIndex] < 58
         if(isdigit(myStr[internalIndex][0])) {
             printf("numeric: %s\n", myStr[internalIndex]);
-            //std::cout << "numeric: " << myStr[internalIndex] << std::endl;
             currentIndex++;
         }
         while (internalIndex == currentIndex) {
@@ -41,9 +40,12 @@ void *alpha(char* myStr[]) {
 
 int main(int argc, char* argv[]) {
     arrayLength = argc;
+
     pthread_t alpha_thread, num_threadl;
+
     pthread_create(&alpha_thread, NULL, reinterpret_cast<void* (*)(void*)>(alpha), argv);
     pthread_create(&num_threadl, NULL, reinterpret_cast<void* (*)(void*)>(numeric), argv);
+    
     pthread_join(alpha_thread, NULL);
     pthread_join(num_threadl, NULL);
     return 0;
